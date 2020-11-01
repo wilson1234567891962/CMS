529365003d76cd48a3b770ffc4f684c4a7091fa8
Lizeth <Lizethgio57@gmail.com>
2020-11-01 17:20:48 -0500

    Adding Categories to the sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,30 +1,9 @@
  <div class="col-md-4">
-               
-               <?php 
-     
-                   if(isset($_POST['submit'])){
-                        $search = $_POST['search'];
-                        $query = "SELECT * FROM posts WHERE post_tags LIKE  '%$search%'";
-                        $search_query = mysqli_query($connection,$query);
-                       
-                        if (!$search_query) {
-                           die("QUERY FAILED" . mysqli_error($connection));
-                        }
-                       
-                        $count = mysqli_num_rows($search_query);
-                       
-                       if ($count == 0) {
-                            echo "<h1>NO RESULT </h1>";
-                       }
-                   }
-               
-                ?>
-               
-               
+            
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <form action="" method="post">
+                    <form action="search.php" method="post">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
@@ -39,42 +18,32 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
+                   
+                 <?php 
+                       $query = "SELECT * FROM categories LIMIT 3";
+                       $select_categories_sidebar = mysqli_query($connection,$query);
+                  ?>
+                   
+                   
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                               
+                               <?php 
+                                  while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                                     $cat_title = $row['cat_title'];
+                                     echo "<li><a href='#'> {$cat_title} </a> </li>";
+                                  }
+                                ?> 
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
+                        
                     </div>
                     <!-- /.row -->
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php  include "widget.php"; ?>
 
 </div>
