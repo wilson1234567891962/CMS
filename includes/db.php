859bcff0792fe8d98e933543ce37bfac5f38bf7b
Lizeth <Lizethgio57@gmail.com>
2020-11-01 12:22:25 -0500

    Fix connection  in databatse

--- includes/db.php
+++ includes/db.php
@@ -16,11 +16,11 @@ $connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
 $query = "SET NAMES utf8";
 mysqli_query($connection,$query);
 
-//if($connection) {
-//
-//echo "We are connected";
-//
-//}
+if($connection) {
+
+echo "We are connected";
+
+}
 
 
 
