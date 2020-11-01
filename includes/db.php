017a32bb2a319356708d181deec752fc7d4889bb
Lizeth <Lizethgio57@gmail.com>
2020-11-01 13:48:49 -0500

    Inserting data into category table and displaying it, also modify the navigation file to include dynamic options

--- includes/db.php
+++ includes/db.php
@@ -11,17 +11,15 @@ define(strtoupper($key), $value);
 
 $connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
 
-
-
 $query = "SET NAMES utf8";
 mysqli_query($connection,$query);
 
-if($connection) {
-
-echo "We are connected";
-
-}
-
+//if($connection) {
+//
+//echo "We are connected";
+//
+//}
+//
 
 
 
