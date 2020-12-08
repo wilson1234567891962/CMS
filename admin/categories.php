9981b03f9377ac8651ec6bdc273c5cb169b60752
Lizeth <Lizethgio57@gmail.com>
2020-12-08 18:02:14 -0500

    Adding the special function to our header file

--- admin/categories.php
+++ admin/categories.php
@@ -28,6 +28,26 @@
                        
                         
                        <div class="col-xs-6">
+                          
+                          <?php 
+                          
+                            if(isset($_POST['submit'])){
+                                $cat_title = $_POST['cat_title'];
+                                if ($cat_title == "" || empty($cat_title)) {
+                                    echo "This field should not be empty";
+                                } else {
+                                    $query = "INSERT INTO CATEGORIES(cat_title) VALUES() "; 
+                                    $query .= "VALUES('{$cat_title}')";
+                                    
+                                    $create_category_query = mysqli_query($connection,$query);
+                                    
+                                    if($create_category_query){
+                                        die('QUERY FAILED' . mysqli_query($connection,$query));
+                                    }
+                                }
+                             }
+                           
+                           ?>
                            
                             <form action="" method="post">
                                <div class="form-group">
